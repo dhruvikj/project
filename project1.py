5e9852022623e93801f523f90990dc1b083fa0e6
dhruvikj <69350509+dhruvikj@users.noreply.github.com>
2020-08-07 18:04:34 +0530

    Add files via upload
    different operations with the list. give the same output as mention.

---
+++ project1.py
@@ -0,0 +1,8 @@
+list1 = [12,-7,5,64,-14]
+for x in list1:
+    if x>0:
+        print(x,end=",")
+
+
+        
+        
---
+++ project2.py
@@ -0,0 +1,6 @@
+list2 = [12,14,-95,3]
+list3 = []
+for y in list2:
+    if y>0:
+        list3.append(y)
+print(list3)  
